b0b053cc9e1d5af12b4c788a409fd32213f4db94
heLomaN <caozhk@gmail.com>
2014-03-10 23:56:19 +0800

    158b_taxi

---
+++ 158b_taxi.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  vector<int> stat(4,0);
+  for(int i = 0;i < n;i++){
+    int tmp;
+    cin >> tmp;
+    stat[tmp-1] += 1;
+  }
+
+  int num_taxi = 0;
+  num_taxi += stat[3];
+  if(stat[2]>= stat[0]){
+    num_taxi += stat[2];
+    num_taxi += (stat[1]+1)/2;
+  }
+  else{
+    num_taxi += stat[2];
+    stat[0] -= stat[2];
+    num_taxi += (stat[0] + stat[1]*2 -1)/4 + 1;
+  }
+
+  cout << num_taxi << endl;
+  return 0;
+}
